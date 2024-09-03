2a984604de974c346a5ea67ae329cac6b9cc3942
Sathwikkoleti <sathwikkoleti@gmail.com>
2024-09-03 13:46:38 +0530

    FirstFive

---
+++ auto-text-effect/Script.js
@@ -0,0 +1,20 @@
+const containerE1 = document.querySelector(".container");
+const careers = ["Youtuber","Web Developer","Engineer"];
+let careerIndex = 0;
+let charecterIndex = 0;
+updateText();
+
+function updateText(){
+    charecterIndex ++;
+    containerE1.innerHTML = `<h1>I am ${careers[careerIndex].slice(0,1) === "E" ? "an":"a"} ${careers[careerIndex].slice(0,charecterIndex)}</h1>`;
+   
+    if(charecterIndex === careers[careerIndex].length){
+        careerIndex ++;
+        charecterIndex = 0;
+    }
+
+    if(careerIndex === careers.length){
+        careerIndex = 0;
+    }
+    setTimeout(updateText,1000);
+}
